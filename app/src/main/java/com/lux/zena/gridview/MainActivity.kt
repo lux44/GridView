f4af18d3fbce89d4f17d64afe212f20b5eddfffe
lux <couquedas14@gmail.com>
2022-07-27 09:00:32 +0900

    떄려치자

--- app/src/main/java/com/lux/zena/gridview/MainActivity.kt
+++ app/src/main/java/com/lux/zena/gridview/MainActivity.kt
@@ -2,11 +2,18 @@ package com.lux.zena.gridview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lux.zena.gridview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding:ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    lateinit var items : MutableList<Item>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
 
         var img = arrayOf(
             R.drawable.sample01,
@@ -25,7 +32,7 @@ class MainActivity : AppCompatActivity() {
             "file"
         )
 
-        val gridViewAdapter = GridViewAdapter(this, img,txt)
+        //val gridViewAdapter = GridViewAdapter(this, img,txt)
 
     }
 }
